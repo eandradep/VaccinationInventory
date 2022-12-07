cd3b8ecac8d9255f70058c9bf734dbbb291ebb70
edisonAndrade1994 <edison.andrade@weconnect.com.ec>
2022-12-06 21:49:52 -0500

    Initial commit
    First project configurations

---
+++ src/test/java/com/kruger/vaccination/inventory/VaccinationInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.vaccination.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
